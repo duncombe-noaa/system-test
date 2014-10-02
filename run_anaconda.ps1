76394276bb41f87ed9f82fdb5377ccedd55fb86e
Chris Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-10-02 14:33:09 -0400

    Commenting script.

--- run_anaconda.ps1
+++ run_anaconda.ps1
@@ -1,3 +1,8 @@
-
+# This stupid script is for getting access to an Anaconda-aware 
+# shell while still in a git posh. I don't have time right now to 
+# fool with figuring adding to paths when I don't have admin rights
+# and am blocked at every turn.
+ 
 C:\Windows\System32\cmd.exe /k "C:\Users\christopher.rae\AppData\Local\Continuum\Anaconda\Scripts\anaconda.bat"
+
 
